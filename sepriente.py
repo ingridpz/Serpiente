c38ddc453b9e14e68eea3eab0ae2d19329cf6bac
Abraham Puente <leoabrahamp@gmail.com>
2020-10-28 13:52:24 -0600

    Corrección al movimiento
    Quitar la comprobación a igual a tomar un nuevo valor

--- sepriente.py
+++ sepriente.py
@@ -55,36 +55,36 @@ def move():
         rn=randrange(7)
         if rn == 0 and food.x <= 180:
             #Movimiento a la derecha
-            food.x == food.x +10
+            food.x = food.x +10
             food.y = food.y
         if rn ==1 and food.x <= 180 and food.y <= 180:
             #Movimiento en digonal
-            food.x == food.x +10
+            food.x = food.x +10
             food.y = food.y + 10
         if rn == 2 and food.x >= -180 and food.y <= 180:
             #Movimiento en diagonal
-            food.x == food.x -10
+            food.x = food.x -10
             food.y = food.y + 10
         if rn == 3 and food.x >= -180:
             #Movimiento a la izquierda
-            food.x == food.x - 10
+            food.x = food.x - 10
             food.y = food.y
         if rn == 4 and food.y <= 180:
             #Movimiento hacia arriba
-            food.x == food.x 
+            food.x = food.x 
             food.y = food.y + 10
         if rn == 5 and food.x >= -180 and food.y >= -180:
             #Movimiento en diagonal
-            food.x == food.x - 10
+            food.x = food.x - 10
             food.y = food.y - 10
         if rn == 6 and food.y >= -180:
             #Movimiento hacia abajo
-            food.x == food.x
+            food.x = food.x
             food.y = food.y -10
         if rn == 7 and food.x <= 180 and food.y >= -180:
             #Movimiento en diagonal
-            food.x == food.x +10
-            food.y == food.y -10 
+            food.x = food.x +10
+            food.y = food.y -10 
 
     clear()
 
